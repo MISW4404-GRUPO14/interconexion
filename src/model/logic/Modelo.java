9df78582c30fc3fae9d4378608706ea9dbf1795b
santiago alejandro salinas vargas <sanalejo1974@gmail.com>
2024-11-16 19:19:54 -0500

    fix: se corrige el casting en el método toString para evitar excepciones y se mejora la legibilidad del código

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -90,6 +90,7 @@ public class Modelo {
 
 	public String toString() {
 		String fragmento = "Info básica:";
+		final String NOMBRE = "\n Nombre: ";
 
 		fragmento += "\n El número total de conexiones (arcos) en el grafo es: " + grafo.edges().size();
 		fragmento += "\n El número total de puntos de conexión (landing points) en el grafo: "
@@ -97,19 +98,18 @@ public class Modelo {
 		fragmento += "\n La cantidad total de países es:  " + paises.size();
 		Landing landing = null;
 		try {
-			landing = (Landing) ((NodoTS) points.darListaNodos().getElement(1)).getValue();
-			fragmento += "\n Info primer landing point " + "\n Identificador: " + landing.getId() + "\n Nombre: "
+			landing = (Landing) ((NodoTS<String, Landing>) points.darListaNodos().getElement(1)).getValue();
+			fragmento += "\n Info primer landing point " + "\n Identificador: " + landing.getId() + NOMBRE
 					+ landing.getName()
 					+ " \n Latitud " + landing.getLatitude() + " \n Longitud" + landing.getLongitude();
 
-			Country pais = (Country) ((NodoTS) paises.darListaNodos().getElement(paises.darListaNodos().size()))
+			Country pais = (Country) ((NodoTS<String, Country>) paises.darListaNodos().getElement(paises.darListaNodos().size()))
 					.getValue();
 
 			fragmento += "\n Info último país: " + "\n Capital: " + pais.getCapitalName() + "\n Población: "
 					+ pais.getPopulation() +
 					"\n Usuarios: " + pais.getUsers();
 		} catch (PosException | VacioException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
